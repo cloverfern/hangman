ae6822a753fed53e697bfa9d42f5486d61a9027c
Fern <frlimaa@gmail.com>
2024-01-25 23:31:33 -0600

    make move wip

--- lib/impl/game.ex
+++ lib/impl/game.ex
@@ -31,4 +31,19 @@ defmodule Hangman.Impl.Game do
     }
   end
 
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game, _guess)
+  when game.state == :won or game.state == :lost do
+    { game, tally(game) }
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list() |> Enum.sort(),
+    }
+  end
+
 end
--- lib/type.ex
+++ lib/type.ex
@@ -1,4 +1,8 @@
 defmodule Hangman.Type do
+  @moduledoc """
+
+  """
+
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @type tally :: %{
     turns_left: integer,
