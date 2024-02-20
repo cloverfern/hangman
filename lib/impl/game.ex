5b3fc7fedba1f42b0789209ea3550baf686e67f8
Fern <frlimaa@gmail.com>
2024-02-19 22:26:39 -0600

    added tally api

--- lib/impl/game.ex
+++ lib/impl/game.ex
@@ -81,7 +81,7 @@ defmodule Hangman.Impl.Game do
     {game, tally(game)}
   end
 
-  defp tally(game) do
+  def tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
